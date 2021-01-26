9a4c507b46343b9f1ba0d7d27c942a01ef03c30d
Albin Wallenius Woxnerud <70270613+AlbinWalleniusWoxnerud@users.noreply.github.com>
2021-01-26 14:55:37 +0100

    26/1-2021
    started working on the js for todolist
    
    no major changes other than the fact that I realised that its easier to create a grid to contain the list instead of a table
    
    added: minor js
    removed: table

---
+++ js.js
@@ -0,0 +1,14 @@
+let toDoList = document.querySelector('#toDoList');
+toDoList.sty
+
+function addTo_list() { 
+    console.log('Hello world')
+}
+
+function removeFrom_list() { 
+console.log('remove world')
+}
+
+function reset_list() { 
+console.log('reset world')
+}
